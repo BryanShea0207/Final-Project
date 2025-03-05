8a01a918892e7669f2507425b2b95ee0021e49a3
BryanShea0207 <Bryanshea0207@gmail.com>
2025-03-05 11:05:24 -0500

    added form to add exercise and card to veiw

--- Midterm/Client/src/App.vue
+++ Midterm/Client/src/App.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
 import NavBar from './components/NavBar.vue';
+import AddExercise from './components/AddExercise.vue';
+import ExerciseSummery from './components/ExerciseSummery.vue';
 </script>
 
 <template>
   <header>
       <NavBar />
   </header>
+  <main>
+    <section class="columns">
+      <div class="column is-one-quarter">
+        <AddExercise />
+      </div>
+      <div class="column is-one-third">
+        <ExerciseSummery />
+      </div>
+    </section>
+  </main>
 </template>
 
 <style scoped>
---
+++ Midterm/Client/src/components/AddExercise.vue
@@ -0,0 +1,123 @@
+ <script setup lang="ts">
+    function showCardio() {
+        const weight = document.getElementById('weight');
+        const cardio = document.getElementById('cardio');
+        if (weight) {
+            weight.classList.add('is-hidden');
+        }
+        if (cardio) {
+            cardio.classList.remove('is-hidden');
+        }
+    }
+
+    function showWeight() {
+        const weight = document.getElementById('weight');
+        const cardio = document.getElementById('cardio');
+        if (weight) {
+            weight.classList.remove('is-hidden');
+        }
+        if (cardio) {
+            cardio.classList.add('is-hidden');
+        }
+    }
+ </script>
+ 
+ <template>
+    <div class="box">
+        <h1 class="title">Exercise Summery</h1>
+        <div class="columns">
+            <div class="column">
+                <div class="tabs">
+                    <ul>
+                        <li @click="showWeight" class="is-active"><a>Weight</a></li>
+                        <li><a @click="showCardio">Cardio</a></li>
+                    </ul>
+                </div>
+                <div id="weight" class="tab-content">
+                <div class="field">
+                    <label class="label">Exercise Name</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Exercise Name">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">Sets</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Sets">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">Reps</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Reps">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">Weight</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Weight">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">Rest Time</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Rest Time">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">Notes</label>
+                    <div class="control">
+                        <textarea class="textarea" placeholder="Notes"></textarea>
+                    </div>
+                </div>
+                <div class="field">
+                    <div class="control">
+                        <button class="button is-primary">Save</button>
+                    </div>
+                </div>
+                </div>
+                <div class="is-hidden tab-content" id="cardio">
+                    <div class="field">
+                        <label class="label">Exercise Name</label>
+                        <div class="control">
+                            <input class="input" type="text" placeholder="Exercise Name">
+                        </div>
+                    </div>
+                    <div class="field" >
+                        <label class="label">Distance</label>
+                        <div class="control">
+                            <input class="input" type="text" placeholder="Distance">
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label class="label">Time</label>
+                        <div class="control">
+                            <input class="input" type="text" placeholder="Time">
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label class="label">Rest Time</label>
+                        <div class="control">
+                            <input class="input" type="text" placeholder="Rest Time">
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label class="label">Notes</label>
+                        <div class="control">
+                            <textarea class="textarea" placeholder="Notes"></textarea>
+                        </div>
+                    </div>
+                    <div class="field">
+                        <div class="control">
+                            <button class="button is-primary">Save</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+ </template>
+ 
+ <style scoped>
+ 
+ </style>
