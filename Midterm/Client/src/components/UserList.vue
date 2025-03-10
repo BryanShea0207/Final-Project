4a0ddd70a94b820fe01f3e99ea4b6bebea060ea9
BryanShea0207 <Bryanshea0207@gmail.com>
2025-03-10 10:24:05 -0400

    added a defult to summery[]

--- Midterm/Client/src/components/UserList.vue
+++ Midterm/Client/src/components/UserList.vue
@@ -1,26 +1,32 @@
 <script setup lang="ts">
-    import { users } from '@/models/user';
-    import { ref } from 'vue';
+import { users } from '@/models/user'
+import { ref } from 'vue'
+import { RouterLink } from 'vue-router'
 
-    const menuToggle = ref(false);
+const menuToggle = ref(false)
 </script>
 
-
 <template>
-    <section>
+  <section>
     <div class="dropdown is-active is-flex is-justify-content-center">
-        <div class="dropdown-trigger">
-            <button class="button is-large" @click="menuToggle = !menuToggle">
-                <span>Users</span>
-            </button>
+      <div class="dropdown-trigger">
+        <button class="button is-large" @click="menuToggle = !menuToggle">
+          <span>Users</span>
+        </button>
+      </div>
+      <div class="dropdown-menu has-buttons are-large">
+        <div
+          class="dropdown-content is-align-content-center"
+          v-for="user in users"
+          v-show="menuToggle"
+        >
+          <div class="dropdown-item">
+            <RouterLink to="/Main" class="button is-capitalized has-text-centered">
+              {{ user.userName }}</RouterLink
+            >
+          </div>
         </div>
-        <div class="dropdown-menu has-buttons are-large">
-            <div class="dropdown-content is-align-content-center" v-for="user in users" v-show="menuToggle">
-                <div class="dropdown-item">
-                    <a class="button is-capitalized has-text-centered"> {{ user.userName }}</a>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
-    </section>
+  </section>
 </template>
--- Midterm/Client/src/models/summary.ts
+++ Midterm/Client/src/models/summary.ts
@@ -1,7 +1,5 @@
 import { ref } from "vue";
 
-export const summaries = ref<Summary[]>([]);
-
 export abstract class Summary {
     name = "";
     note = "";
@@ -47,6 +45,8 @@ export class SummaryCardio extends Summary{
       const pace = this.time / this.distance;
       return Math.floor(pace / 60) + '\'' + pace % 60 + '\"';
     }
-
 }
 
+export const summaries = ref<Summary[]>([
+  new SummaryWeight("curls", 5, 3, 120, "max heart rate: 130")
+]);
