07b60467d138f9f1b42dd8f96d876aff35c84f00
BryanShea0207 <Bryanshea0207@gmail.com>
2025-03-20 15:43:51 -0400

    implemented admin control buttons

--- Midterm/Client/src/components/AdminTable.vue
+++ Midterm/Client/src/components/AdminTable.vue
@@ -1,10 +1,44 @@
 <script setup lang="ts">
 import { getAll, getOne } from '@/models/user'
-</script>
+import { ref} from 'vue'
+import WeightSummary from './WeightSummary.vue'
+import CardioSummary from './CardioSummary.vue'
+import SocialPost from './SocialPost.vue'
 
-<script lang="ts">
+const showPosts = ref(false);
+const showFriends = ref(false)
+const showSummeries = ref(false);
+const selectedUser = ref(0);
+
 function deleteAccount(userId: number) {
+  if (getOne(userId).userName){
+    getOne(userId).userName = ""
+  }
   getAll().users.splice(userId)
+}
+
+function viewPosts(userId: number){
+  showSummeries.value = false
+  showFriends.value = false
+  showPosts.value = true
+  
+  selectedUser.value = userId
+}
+
+function viewFriends(userId: number){
+  showPosts.value = false
+  showSummeries.value = false
+  showFriends.value = true
+
+  selectedUser.value = userId
+}
+
+function viewSummeries(userId: number){
+  showPosts.value = false
+  showFriends.value = false
+  showSummeries.value = true
+
+  selectedUser.value = userId
 }
 </script>
 
@@ -23,14 +57,40 @@ function deleteAccount(userId: number) {
           <td>
             <div class="buttons">
               <button class="button is-danger" @click="deleteAccount(user.userId)">delete</button>
-              <a class="button is-primary">View Posts</a>
-              <a class="button is-primary">View Friends</a>
-              <a class="button is-primary">View Summaries</a>
+              <a class="button is-primary" @click="viewPosts(user.userId)">View Posts</a>
+              <a class="button is-primary" @click="viewFriends(user.userId)">View Friends</a>
+              <a class="button is-primary" @click="viewSummeries(user.userId)">View Summaries</a>
             </div>
           </td>
         </tr>
       </tbody>
     </table>
+  </div>
+  <div class="container pt-5" v-if="showPosts" v-for="post in getOne(selectedUser).posts">
+    <SocialPost :post="post">
+      <WeightSummary v-if="post.summary.type === 'weight'" :data="post.summary" />
+      <CardioSummary v-else :data="post.summary"> </CardioSummary>
+    </SocialPost>
+  </div>
+  <div class="container pt-5" v-if="showFriends">
+    <table class="table">
+      <thead>
+        <tr>
+          <td>userName</td>
+          <td>userId</td>
+        </tr>
+      </thead>
+      <tbody>
+      <tr v-for="friend in getOne(selectedUser).friendsIds">
+        <td>{{ getOne(friend).userName }}</td>
+        <td>{{ friend }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+  <div class="container pt-5 " v-if="showSummeries" v-for="summary in getOne(selectedUser).summaries">
+    <WeightSummary v-if="summary.type === 'weight'" :data="summary" />
+    <CardioSummary v-else :data="summary" />
   </div>
 </template>
 
