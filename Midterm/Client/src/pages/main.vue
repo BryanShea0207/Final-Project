a675b232282ee478612a44a56703ef43d7f13e79
BryanShea0207 <Bryanshea0207@gmail.com>
2025-04-07 07:56:48 -0400

    added social feed of friends posts

--- Midterm/Client/src/pages/main.vue
+++ Midterm/Client/src/pages/main.vue
@@ -5,6 +5,17 @@ import CardioSummary from '../components/CardioSummary.vue'
 import SocialPost from '@/components/SocialPost.vue'
 import { currentUser } from '@/components/UserList.vue'
 import FriendsList from '@/components/FriendsList.vue'
+import { ref } from 'vue'
+import type { Post } from '@/models/posts'
+import { getOne } from '@/models/user'
+
+const posts = ref<Post[]>([]);
+
+let ids = currentUser.value?.friendsIds;
+ids?.forEach(friend => {
+  getOne(friend).posts?.forEach(post => {posts.value.push(post)});
+});
+posts.value.sort((a,b) => Date.parse(b.date)-Date.parse(a.date))
 </script>
 
 <template>
@@ -14,14 +25,13 @@ import FriendsList from '@/components/FriendsList.vue'
         <AddExercise />
       </div>
       <div class="column is-half">
-        <div class="container py-3" v-for="post in currentUser?.posts">
+        <div class="container py-3" v-for="post in posts">
           <SocialPost :post="post">
             <WeightSummary v-if="post.summary.type === 'weight'" :data="post.summary">
             </WeightSummary>
             <CardioSummary v-else :data="post.summary"> </CardioSummary>
           </SocialPost>
         </div>
-        {{ currentUser?.posts[1] }}
       </div>
       <div class="column" style="height: 100%;">
         <div class="panel p-5" style="height: 100%">
