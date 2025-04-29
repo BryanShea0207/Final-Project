f796225773ac913fde407ad2a4f18c0c6e87fa1a
BryanShea0207 <Bryanshea0207@gmail.com>
2025-04-29 09:43:19 -0400

    Update myFetch.ts

--- client/src/models/myFetch.ts
+++ client/src/models/myFetch.ts
@@ -6,6 +6,9 @@ export function rest<T>(url: string): Promise<T> {
   
 export function api<T>(action: string): Promise<T> {
     console.log("calling rest() on " + action)
-    return rest<T>(`${API_ROOT}${action}`)
+    return rest<T>(`${API_ROOT}${action}`).then((item) => {
+        console.log("rest() called on " + action)
+        return item
+    })
 }
 
