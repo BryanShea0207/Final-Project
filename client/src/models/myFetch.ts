5be88030a86ee87d7679a8b3db6b5f79688fd89f
BryanShea0207 <Bryanshea0207@gmail.com>
2025-04-29 09:21:27 -0400

    Update myFetch.ts

--- client/src/models/myFetch.ts
+++ client/src/models/myFetch.ts
@@ -2,6 +2,7 @@ export const API_ROOT = (import.meta.env.VITE_API_ROOT as string)
 
 export function rest<T>(url: string): Promise<T> {
     return fetch(url).then((x) => {
+        console.log(url)
         console.log(x)
         return x.json()
     })
