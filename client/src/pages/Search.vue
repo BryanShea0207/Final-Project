d644315771e9bf23ecc0846068ad63db190dde27
BryanShea0207 <Bryanshea0207@gmail.com>
2025-05-15 14:30:59 -0400

    Update Search.vue
    Used Oruga to add auto complete to search

--- client/src/pages/Search.vue
+++ client/src/pages/Search.vue
@@ -3,17 +3,39 @@ import { getAll, type User } from '@/models/user';
 import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { ref } from 'vue';
+import type { OptionsProp } from "@oruga-ui/oruga-next";
 
-const allUsers = ref<User[]>([])
+const usersList = ref<User[]>([])
+const userOptions = ref<User[]>([])
+const options: OptionsProp = getAll()
+console.log(options)
+getAll().then((users) => {
+  usersList.value = users
+})
 
-getAll().then((users) => allUsers.value = users)
+const selected = ref();
+
+
+async function convertUsers(users:Users[]) {
+  const formatedUsers = []
+  users.forEach(user => {
+    formatedUsers.push({label: user.first_Name + " " + user.last_Name, value: user})
+  });
+  return formatedUsers
+}
 </script>
 
 <template>
     <div class="container">
+      <o-autocomplete
+            v-model="selected"
+            :options="options"
+            placeholder="Find a name..."
+            open-on-focus>
+        </o-autocomplete>
     <div class="fixed-grid has-4-cols">
   <div class="grid">
-    <div class="cell" v-for="user in allUsers">
+    <div class="cell" v-for="user in usersList">
         <RouterLink :to="`/Profile/${user.user_id}`">
             <div class="card">
                 <FontAwesomeIcon :icon="faCircleUser" size="4x"/>
